47a49a52025009e07033b0eaced18a8e6c07aee0
LuizFGRocha <luizfgrocha04@gmail.com>
2026-06-02 10:38:00 -0300

    feat: implement repository traversal with pydriller

--- jamal/collector.py
+++ jamal/collector.py
@@ -1,11 +1,25 @@
+from pydriller import Repository
+
 from jamal.models import CommitInfo, FileChange
 
 
 class Collector:
-    """Collects commit history from a git repository."""
+    """Collects commit history from a git repository using pydriller."""
 
     def __init__(self, repo_path: str) -> None:
         self.repo_path = repo_path
 
     def collect(self) -> list[CommitInfo]:
-        raise NotImplementedError
+        commits = []
+        for commit in Repository(self.repo_path).traverse_commits():
+            commits.append(
+                CommitInfo(
+                    hash=commit.hash,
+                    author=commit.author.name,
+                    author_email=commit.author.email,
+                    date=commit.author_date,
+                    message=commit.msg,
+                    files_changed=[],
+                )
+            )
+        return commits
