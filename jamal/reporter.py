cbbb2f9a936ef1d35c023a460fe4240a1a591e95
vlcfaria <artixsproductions@gmail.com>
2026-06-08 14:26:00 -0300

    feat: add progress bar during analysis

--- jamal/reporter.py
+++ jamal/reporter.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from rich import box
 from rich.console import Console
 from rich.panel import Panel
+from rich.progress import track
 from rich.table import Table
 from rich.text import Text
 
